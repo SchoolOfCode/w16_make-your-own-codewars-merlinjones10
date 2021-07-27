2b702a580122d6766736c1a304af232f6ead0b3e
merlinjones10 <merlinjones10@gmail.com>
2021-07-27 11:22:47 +0100

    first check working in jest

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 /* 
 👉 Write your kata here!
+Gregg needs to stay below 200 calories for lunch. Write a function that returns true if the food item is less than 300 {
+    the food item is a key value pair with name and the calories e.g {chips: 400}
+}
 */
 
+// let testFood = {
+//   name: "chips",
+//   calories: "100",
+// };
+
 //👉 Write the function your CodeWarriors will start with below here:
+function calorieCheck(foodItem) {
+  if (foodItem.calories < 300) {
+    return true;
+  }
+  return false;
+}
+
+// console.log(calorieCheck(testFood));
+// export default calorieCheck;
+module.exports = calorieCheck;
--- main.test.js
+++ main.test.js
@@ -1,1 +1,8 @@
 //👉 Write your tests below here:
+// const { default: calorieCheck } = require("./main");
+const calorieCheck = require("./main");
+
+const testItem = { name: "chips", calories: 200 };
+test("returns true if an objects calorie id is lower than 300", () => {
+  expect(calorieCheck(testItem)).toBe(true);
+});
