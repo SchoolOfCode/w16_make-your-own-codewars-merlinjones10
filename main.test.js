b6ad5f277703e33a25e91b9052b84ee5a0cc6882
merlinjones10 <merlinjones10@gmail.com>
2021-07-28 13:28:49 +0100

    multiple test case function working

--- main.test.js
+++ main.test.js
@@ -3,11 +3,27 @@
 const calorieCheck = require("./main");
 
 const testItem = { name: "chips", calories: 200 };
+//
 test("returns true if an objects calorie id is lower than 300", () => {
   expect(calorieCheck(testItem)).toBe(true);
 });
 
-// const testItem = { name: "chips", calories: 200 };
 test("returns true if an objects calorie id is lower than 300", () => {
   expect(calorieCheck(testItem)).toBeTruthy();
+});
+
+const cases = [
+  [{ name: "muffin", calories: 100 }, true],
+  [{ name: "burger", calories: 600 }, false],
+  [{ name: "salad", calories: "50" }, true],
+  [{ name: "deep fried pizza", calories: 2000 }, false],
+];
+describe("calorie checker, returns true if under 300", () => {
+  test.each(cases)(
+    "given an object as an argument, returns true if that objects calorie id is under 300",
+    (object, expectedResult) => {
+      const result = calorieCheck(object);
+      expect(result).toEqual(expectedResult);
+    }
+  );
 });
